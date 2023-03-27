e0655caeec6ef816ae592952d111ea2eefe3e94d
Anh Nguyet Vu <anngvu@gmail.com>
2023-03-27 09:13:06 -0600

    Modify javascript files

--- schema-viz/js/PrepareRender.js
+++ schema-viz/js/PrepareRender.js
@@ -1,29 +1,10 @@
-$.get('files/config.yml').done(function (data) {
-    //loading content of the config file
-    var config_content = jsyaml.load(data)
-
-    //check the schema name
-    var schema = config_content["schema"]
-
-    ////////////////for using APIs
-    // getRequestedJson(schema).then(tangled_tree_data => {
-    //     console.log('api tangled_tree_data', tangled_tree_data)
-    //     var chart_dta = chart(tangled_tree_data);
-    //     createCollapsibleTree(chart_dta, schema)
-    // })
-    //////////////////////////////
-
-    //////////////////for using static files
-    if (schema == "HTAN") {
-        var tangled_tree_data = parseJSON('files/JSON/HTAN_tangled_tree.json');
-    }
-
+//return an array based on user selection 
+    var schema = 'NF';
+    var tangled_tree_data = parseJSON('files/nf-tools-db.json');
     tangled_tree_data.then(tangled_tree_dta => {
         //get tangle tree layout
         var chart_dta = chart(tangled_tree_dta);
 
         //draw collapsible tree
         createCollapsibleTree(chart_dta, schema);
-    })
-
-})
+    });
